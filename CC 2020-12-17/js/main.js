e24c9b14732dd9cde79ced303084d8c786687e8c
Ruth Reed <ruthreed.dev@gmail.com>
2020-12-17 12:38:02 -0700

    Add coding files for coding challenge 12-17-2020.

---
+++ CC 2020-12-17/js/main.js
@@ -0,0 +1,13 @@
+//Create a function that takes in 3 numbers. The function should return the first number multiplied by the second and then subtract the third. Console.log() the resulting value.
+
+document.querySelector('#input').addEventListener('enter', multiply)
+
+function multiply(a, b, c) {
+    return (a * b) - c
+}
+
+console.log
+
+//Do (a*b)-c
+
+//Return the value via console.log
