ce3380d9926ae05de2f50ed650bb152ad79b8240
Ruth Reed <ruthreed.dev@gmail.com>
2021-01-13 10:05:53 -0700

    Add coding challenge for 1-13-21.
    ~

---
+++ CC 2021-1-13/js/main.js
@@ -0,0 +1,8 @@
+//Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+function firstValueInArray(array) {
+    console.log(array[0] + array[array.length - 1]);
+}
+firstValueInArray([1,4,2,33])
+
+//    first - (array[0])
+//    last - array[array.length-1]
