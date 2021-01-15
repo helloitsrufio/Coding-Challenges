70e5c56457b6d7e3671689bb4ad33901e4bc9d7c
Ruth Reed <ruthreed.dev@gmail.com>
2021-01-15 11:04:30 -0700

    Add coding challenge for 1-15-21.

---
+++ CC 2021-3-22/app.js
@@ -0,0 +1,6 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s){
+    return(( h * 3600000)+(m * 60000)+(s * 1000))
+  }  
