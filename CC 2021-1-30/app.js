6d008fabf81fc5bcfd89144aebd49347790e5cf2
Ruth Reed <ruthreed.dev@gmail.com>
2021-01-30 09:30:31 -0700

    Add coding challenge for 1-30-2021.

---
+++ CC 2021-1-30/app.js
@@ -0,0 +1,18 @@
+//Very simple, given a number, find its opposite.
+//Examples:
+//1: -1
+//14: -14
+//-34: 34
+
+function opposite(n){
+    return n * -1
+  }
+  
+  //below is my old answer (attempt). I figured this out in a MINUTE on the 30 of March. It's probably been a few weeks to a month since I first tried this. So proud of how fast I knew how to solve it!
+  // function opposite(n1) {
+  //   let answer = []
+  //   for (a of answer){
+  //     a * -1
+  //   }
+  // }
+  // opposite(14,1,12)
