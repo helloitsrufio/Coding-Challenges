c6002e5ce2a38a93770e7abb0a47cbd25a28e3c4
Ruth Reed <ruthreed.dev@gmail.com>
2021-03-12 15:57:08 -0700

    Add unsolved coding challenge for 3-12-21.

---
+++ CC 2021-3-12/app.js
@@ -0,0 +1,11 @@
+// Given a string of characters, return the character that appears the most often.
+
+// describe("Max Character", () => {
+//  it("Should return max character", () => {
+//   assert.equal(max("Hello World!"), "l");
+//  });
+// });
+
+// No String or Array Methods (well brute force it first, but then no methods)! 
+
+// From: https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
