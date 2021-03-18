a3929c5c697e5285609e006ff2c0c7b9e263b3fa
Ruth Reed <ruthreed.dev@gmail.com>
2021-03-18 14:57:21 -0600

    Add unsolved coding challenge for 3-19-21.

---
+++ CC 2021-3-19/app.js
@@ -0,0 +1,23 @@
+// Make sure you understand these before taking a look at the question:
+// Objects (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects)
+
+// 👆🏾  Make sure you remember/review bracket notation for objects!  You can check if a property exists and increment a value tied to that property HINT HINT!
+
+    // Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+    // What if the string is empty? Then the result should be empty object literal, {}.
+
+// From: https://www.codewars.com/kata/52efefcbcdf57161d4000091
+
+
+
+
+
+
+
+
+
+
+
+
+// Hint: loop through the characters and use bracket notation to add one to the value for that letter in the object or add that letter as a property of the object with the value of 1
