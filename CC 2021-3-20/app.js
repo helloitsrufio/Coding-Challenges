fb3b3c2fde19e379d1ffd582be72dc5cdda115fb
Ruth Reed <ruthreed.dev@gmail.com>
2021-03-20 09:09:18 -0600

    Add coding challenge for 3/20/21.

---
+++ CC 2021-3-20/app.js
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string passed into it.
+//'world'  =>  'dlrow'
+
+function solution(str){
+    return str.split("").reverse("").join("")
+   }
+   solution('world','hello')
