324b53941839b642ed471e6555775eb1202eb281
Ruth Reed <ruthreed.dev@gmail.com>
2021-03-25 17:09:25 -0600

    Add coding challenge for 3-25-2021.

---
+++ CC 2021-3-25/app.js
@@ -0,0 +1,8 @@
+//The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+//Task :
+//Given a year, return the century it is in.
+
+function century(year) {
+    return Math.ceil(year/100)
+   }
+     
