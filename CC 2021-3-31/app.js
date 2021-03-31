cce56b4ab5e7801c38c277fbca3c7d15f3d3f738
Ruth Reed <ruthreed.dev@gmail.com>
2021-03-31 10:45:25 -0600

    Add coding challenge for 3-31-21.

---
+++ CC 2021-3-31/app.js
@@ -0,0 +1,11 @@
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+//Examples:
+//123 --> "123"
+//999 --> "999"
+
+function numberToString(num) {
+    return num.toString()
+  }
+  
+  
