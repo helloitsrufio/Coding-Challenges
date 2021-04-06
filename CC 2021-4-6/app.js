093ab273f85894b0410f30995d8c814cd9d08838
Ruth Reed <ruthreed.dev@gmail.com>
2021-04-06 12:02:48 -0600

    Add coding challenge for 4-6-21.

---
+++ CC 2021-4-6/app.js
@@ -0,0 +1,8 @@
+// Write a function called repeat_string which repeats the given string str exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+ return s.repeat(n)
+}
