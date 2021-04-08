d284b07f013c5cb8f08bf3939498ceed2195f123
Ruth Reed <ruthreed.dev@gmail.com>
2021-04-08 12:37:18 -0600

    Add coding challenge attempt for 4-8-21 coding challenge.

--- CC 4-8-21/app.js
+++ CC 4-8-21/app.js
@@ -12,3 +12,33 @@
 
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
+
+//P: Does it take in anything besides nums? Special characters? Anything besides an array?
+//R: T o false
+//E: See console logs
+//P: Make function that takes in array
+  // function appearsTwice(arr){
+  //   for(i=0;i<arr.length;i++){
+  //     if(arr.indexOf(i) == arr.lastIndexOf(i)){
+  //       return true
+  //     }else{
+  //     return false
+  //   }
+  //   }
+  // }
+
+  //------------coding challenge attempt----------
+
+  function appearsTwice(arr){
+    let elMap = {},
+    for(const el of arr){
+      elMap[el] == elMap[el] + 1 || 1
+    }
+    if (elMap[el] > 1){
+      return true
+    }else{
+      return false
+    }}
+
+console.log(appearsTwice([1,2,3,3,3,4]), true)
+console.log(appearsTwice([3,4,21]), false)
