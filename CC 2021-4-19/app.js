5ba77016f0ed13576734061057ffd784ee8bd305
Ruth Reed <ruthreed.dev@gmail.com>
2021-04-19 15:04:26 -0600

    Add coding challenge for 4-19-21.

---
+++ CC 2021-4-19/app.js
@@ -0,0 +1,17 @@
+// // Given a string of words, you need to find the highest scoring word.
+
+// // Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// // You need to return the highest scoring word as a string.
+
+// // If two words score the same, return the word that appears earliest in the original string.
+
+// // All letters will be lowercase and all inputs will be valid.
+
+// // Test.assertEquals(high('man i need a taxi up to ubud'), 'taxi');
+// // Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano'); 
+// // Test.assertEquals(high('take me to semynak'), 'semynak');
+
+//Hint: Remember our past problem that used an alphabet string. Think about reduce, indexOf, and creating a function to check the score then looping through your words!
+
+// From: https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
