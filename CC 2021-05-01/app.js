49cb316c75293137069a32d36376f3eb303f4b3f
Ruth Reed <ruthreed.dev@gmail.com>
2021-05-01 17:35:39 -0600

    Add coding challenge for 5-1-21.

---
+++ CC 2021-05-01/app.js
@@ -0,0 +1,40 @@
+//Class coding challenge
+
+//Create a function that returns the capitalization of a name. No arrays, no regex!
+//leon -> Leon
+//bob -> bob
+// John -> John
+
+
+
+//Parameters
+
+
+//Results
+
+
+//Examples
+
+
+//Pseudocode
+
+//Classmate's code. I interviewed.
+
+function nameO(str) {
+    return str.charAt(0).toUpperCase().concat(str.slice(1, str.length-1))
+}
+
+nameO('jennifer')
+"Jennife"
+
+//Class soln:
+
+function cap(str){
+    //Can't just edit the first letter because strings are immutable. So need to create/return a new string. You can do this by adding two strings together. 
+    return str[0].toUpperCase + str.slice(1)
+    // str.slice(1): Will take everything after the first index. You will add this remainder to the capitalized letter in a new string. 
+}
+console.log(cap('leon'))
+
+
+//Would like to try to work through this myself. Slice is confusing to me. 
