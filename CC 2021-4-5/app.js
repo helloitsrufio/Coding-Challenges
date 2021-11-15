18cf35b30e8bb90d5a38bc6c9ee0e39e292793cf
Ruth Reed <ruthreed.dev@gmail.com>
2021-11-15 15:13:36 -0700

    Add (unsolved) coding challenge for 4-5-21.

---
+++ CC 2021-4-5/app.js
@@ -0,0 +1,5 @@
+// Easy one since it is late and just in case... Make sure you talk through prep and Big O! 
+
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+
+// From: https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
