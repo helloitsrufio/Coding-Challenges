eab32e7bf3bc2bb14b8a649f50b6ce1e90c394ef
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-04 17:43:39 -0700

    Add coding challenge for 2-4-22.

---
+++ CC 2022-02-04/app.js
@@ -0,0 +1,10 @@
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+function palindrome(str){
+    let newStr = str.split('').reverse().join('')
+    if(str === newStr){
+        alert('It be a palindrome yeeee')
+    }
+}
+console.log(palindrome('anna'))
+console.log(palindrome('beaver'))
