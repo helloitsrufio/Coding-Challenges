5cc0104e82057c0e288e141d28fd961c75474238
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-06 17:46:22 -0700

    Add coding challenge and solution for 2-6-22.

---
+++ CC 2022-02-06/app.js
@@ -0,0 +1,7 @@
+//Create a function that takes string
+//Print the reverse of that string to the console
+function reverse(str){
+    return str.split('').reverse().join('')
+}
+
+console.log(reverse('I am a bear'))
