7e0517a9cba2cd0cf2679c2a969b92bc0b6c1ea7
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-09 17:48:57 -0700

    Add coding challenge and solution for 2-9-22.

---
+++ CC 2022-2-9/app.js
@@ -0,0 +1,8 @@
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function arrOfNums(arr){
+    for(i=0;i<arr.length;i++){
+        return arr.map(Math.pow(arr[i])
+    }
+}
+//Didn't get it to work in the time frame that I assigned for myself.
