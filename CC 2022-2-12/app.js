f96d491a3255ecfabf6b14ce1774e7ae2816107c
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-12 17:52:44 -0700

    Add coding challenge attempt for 2-12-22 coding challenge.

---
+++ CC 2022-2-12/app.js
@@ -0,0 +1,7 @@
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+function sum(arr){
+    for(i=0;i<arr.length;i++){
+        return arr.map(arr[i]+=)
+    }
+}
+sum([1,2,3,4,5,6])
