cd945bb2055599a3439273a2596a7195b4e2aafc
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-14 17:57:40 -0700

    Add different sort of coding challenge for 2-14-22.

---
+++ CC 2022-2-14/app.js
@@ -0,0 +1,13 @@
+//Create a dog object that has four properties and three methods
+let dog {
+    'hair':true,
+    'nose':true,
+    'feathers':false,
+    'shoes': false,
+
+    scratch(),
+    bark(),
+    sleep()
+
+}
+//Attempt at this challenge
