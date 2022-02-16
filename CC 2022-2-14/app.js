ee40b3a3991d61bd69d041d41b24652e69982711
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-15 18:00:21 -0700

    Add teacher solution to 2-14 coding challenge.

--- CC 2022-2-14/app.js
+++ CC 2022-2-14/app.js
@@ -11,3 +11,21 @@ let dog {
 
 }
 //Attempt at this challenge
+
+//Teacher soln
+let dog = {}
+
+dog.breed = 'poodle'
+dog.color = 'golden'
+dog.legs = 4
+dog.name = 'Fido'
+
+dog.bark = function(){
+    console.log('WOOF')
+}
+dog.jump = function(){
+    console.log('BLAST OFF')
+}
+dog.fetch = function(){
+    console.log('ZOOOOOM')
+}
