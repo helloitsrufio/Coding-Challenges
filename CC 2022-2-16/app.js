17908627eb7e19cfe898c844915fb783e735c58a
Ruth Reed <ruthreed.dev@gmail.com>
2022-02-16 18:35:42 -0700

    Add coding challenge attempt for 2-16 coding challenge.

---
+++ CC 2022-2-16/app.js
@@ -0,0 +1,10 @@
+// Declare a variable and assign it to a sentence as a string. Alert if the sentence is a question.
+let str = 'RUFIO RUFIO RUFIOOOOOOO'
+
+function checkQuestion(str){
+    str.split('')
+    if((RegExp/[?]/g)){
+        alert("It's a question yoooo")
+    }
+}
+//Yeah so...this does not work. It doesn't I thought maybe the RegEx would work but it didn't.
