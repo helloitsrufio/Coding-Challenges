6fce980e151fb53bb6b4a14025a7f3c512f3aead
Ruth Reed <ruthreed.dev@gmail.com>
2022-03-11 16:27:17 -0700

    Add coding challenge attempt for 3-9-22 coding challenge.

--- CC 2022-3-9/app.js
+++ CC 2022-3-9/app.js
@@ -5,3 +5,32 @@
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
 // Note: The function accepts an integer and returns an integer
+
+// -----my attempt-----
+
+//What does it mean to concatenate? Push all integers together into one, but don't add. Will we ever been given an empty num? Or special characters? 
+//Should return the original number, but each number squared, and then all those numbers pushed together (concatenated)
+
+//Make a function that takes in a num (param)
+function concat(num){
+    //Make the number a string
+    let newNum = String(num).split('')
+    //split number into an array
+
+    //change string nums back to reg nums
+    for(i=0;i<newNum.length;i++){
+         String(Math.pow(newNum[i],2))
+    }
+    //square nums
+
+    //turn nums back to string
+    return Number(newNum.join(''))
+    //join num strings together and return.
+}
+//all right. The problem I'm having here is this. I am getting everything right up to line 20. It is taking each number, squaring it, and turning it into a string. If I try to assign this to a new var and push these perf numbers there, it doesn't work. 
+
+//When I try to return the newNum with all the strings joined and turned back to a number, it literally just returns the input. It's v frustrating.
+
+console.log(concat(1234),14916)
+console.log(concat(556),252536)
+console.log(concat('x'),err)
