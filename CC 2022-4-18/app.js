03302398857f159500b146415d2d5985e1846604
Ruth Reed <ruthreed.dev@gmail.com>
2022-04-17 10:51:12 -0600

    Add unsolved coding challenge for 4-18-22 coding challenge.

---
+++ CC 2022-4-18/app.js
@@ -0,0 +1,14 @@
+// /*
+// Simple code review challenge. Guess the output, then run it and see what it actually is.
+// */
+// const AGE = 25;
+// const MAX_AGE = 20;
+
+// // if the condition fails, run this function
+// const failFunction = (() => console.log('Old'))
+
+// // if the condition succeeds, run this function
+// const successFunction = (() => console.log('Young'))
+
+// // Do a check to see if the age is less than or equal to the max age and run the appropriate function
+// (AGE <= MAX_AGE) === true ? successFunction() : failFunction()
