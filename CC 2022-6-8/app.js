b6170b5198db8d9558fad180c11302b12b9f97e9
Ruth Reed <ruthreed.dev@gmail.com>
2022-06-08 12:57:48 -0600

    Add first attempt at 6-8 coding challenge.

--- CC 2022-6-8/app.js
+++ CC 2022-6-8/app.js
@@ -6,3 +6,36 @@
 // Example
 
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+//P: If input is empty/null, return empty arr. We want the count of positive numbers, and the sum of negative nums. 0 is neither + or -.
+//R: Array with: [Count of + nums and sum of - nums]
+//E:
+//P: Make a function that takes in an arr
+
+
+//first attempt
+function countPositiveSumNegative(arr){
+    let count = 0
+    let sum
+    //is it positive or negative?
+    
+    arr.map(e=>{
+        if(e>0){
+            count +1
+        }else if(e<0){
+            sum += e
+        }
+    }
+        )
+        return [count,sum]
+    //if it's positive, count how many positive numbers there are.
+    //if it's negative, sum the negative numbers together.
+    //if it's empty or null, return empty arr
+    //put them into an arr
+    //return that arr
+}
+
+//Idk what is going on here, it is not doing literally anything we tell it too woof. I feel like this should work
+console.log(countPositiveSumNegative([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10, -65])
+console.log(countPositiveSumNegative([1, 7, 8, 9, -1, -12, -94 -15]),[4, -122])
+console.log(countPositiveSumNegative([]),[])
