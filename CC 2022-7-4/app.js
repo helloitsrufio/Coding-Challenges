e69c4a1c009fbe428aefc8c9afb439c5a4319851
Ruth Reed <ruthreed.dev@gmail.com>
2022-07-04 10:54:22 -0600

    Add first attempt to solve 7-4 code challenge.

--- CC 2022-7-4/app.js
+++ CC 2022-7-4/app.js
@@ -3,3 +3,35 @@
 // Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
 
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+//Function needs to take in a num as an input. Then it needs to return the # of bits that are = to 1 in the binary representation of that num. The num will always be non-negative. So basically it's counting ones in the binary representation of the num param.
+//Return a number (the num of 1s in the binary num)
+//Make a function that takes in a num
+
+//----------first attempt----------
+function countBits(num){
+    if(num === 1){
+        return 1
+    }else if(num === 0){
+        return '0'
+    }
+    //if the number is non negative, then
+    else if(num > 0) 
+        {num === 1{
+            return '1'
+        }
+        ||
+        num === 0{
+            return '0'
+        }
+        //if the num is 1, return '1'
+        //if the num is 0, return '0'
+        //else do Math.floor(num/2) and add num % 2
+    //return}
+}}
+
+//ok, so this is not working and I actually am not sure how it's supposed to work. This is reminiscent of a convert-to-binary soln I found online. I just don't get it, so I'm going to try a different approach. 
+
+console.log(countBits(1234),5)
+console.log(countBits(1.23),28)
+console.log(countBits(5),2)
