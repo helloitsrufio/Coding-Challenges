1e1d1633d35c81b62674432f2323e7da0da1fd36
Ruth Reed <ruthreed.dev@gmail.com>
2022-07-05 11:07:36 -0600

    Add coding challenge solution for 7-4 coding challenge.

--- CC 2022-7-4/app.js
+++ CC 2022-7-4/app.js
@@ -32,6 +32,17 @@ function countBits(num){
 
 //ok, so this is not working and I actually am not sure how it's supposed to work. This is reminiscent of a convert-to-binary soln I found online. I just don't get it, so I'm going to try a different approach. 
 
+//----------second attempt----------
+function countBits(num){
+    //convert to decimals by doing num.toString (this works because the method will convert the num you give it to a string, and I guess this means it turns it to a binary num when you put 2 in as the radix?) and set it equal to a different val
+    const binary = num.toString(2)
+    let splitBinary = binary.split('')
+    return splitBinary.filter(e=>e == '1').length
+
+}
 console.log(countBits(1234),5)
 console.log(countBits(1.23),28)
 console.log(countBits(5),2)
+
+//So I originally thought I'd have to use a count var, but that actually didn't end up working out. I had to set the binary arr as a var. Then I was trying to map through and apply a condition, but that wasn't working out for me either. I realized that I just wanted to count the ones, and I could do that if I filtered out everything that wasn't a one. If I tacked on a .length to the end, then the count var was completely redundant.
+//The trickiest thing here was trying to make something a binary num. But it's fairly simple, even if I still don't understand it. 
