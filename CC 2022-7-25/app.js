4e681701b6401ae28f75f90794313fcec4df7cc6
Ruth Reed <ruthreed.dev@gmail.com>
2022-07-24 16:11:13 -0600

    Add unsolved coding challenge.

---
+++ CC 2022-7-25/app.js
@@ -0,0 +1,15 @@
+//https://www.structy.net/problems/linked-list-values
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListValues = (head) => {
+  // todo
+};
+
+module.exports = {
+  linkedListValues,
+};
