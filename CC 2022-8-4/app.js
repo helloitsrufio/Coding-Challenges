1164e04410ec0fa558918b1b8509256e50bba4ee
Ruth Reed <ruthreed.dev@gmail.com>
2022-08-04 10:13:25 -0600

    Add solved coding challenge for 8-4-22.

--- CC 2022-8-4/app.js
+++ CC 2022-8-4/app.js
@@ -6,3 +6,22 @@
 // Input: 145263 Output: 654321
 
 // Input: 123456789 Output: 987654321
+
+
+//P: Takes in an integer (is it always an integer?). Non-negative integer; does that include 0? Should return it with digits in descending order.
+//R: An integer with #s in descending order.
+//E: 
+//P: Make a function that takes in an integer
+function descendingOrder(int){
+    //Grab integer and make it into a string
+    let string = int.toString().split('').sort((a,b)=>b-a).join('')
+    //Split string by each character
+    //Sort
+    //Put string back together
+    //Make it back into an integer
+    return Number(string)
+}
+
+console.log(descendingOrder(42145),54421)
+console.log(descendingOrder(145263),654321)
+console.log(descendingOrder(123456789),987654321)
