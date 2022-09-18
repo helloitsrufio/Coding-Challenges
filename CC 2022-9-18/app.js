413e9b67ed764a52af95b30428ff4b559540a252
Ruth Reed <ruthreed.dev@gmail.com>
2022-09-18 09:46:14 -0600

    Add solution for 9-18 coding challenge.

--- CC 2022-9-18/app.js
+++ CC 2022-9-18/app.js
@@ -5,3 +5,22 @@
 
 // 35231 => [1,3,2,5,3]
 // 0 => [0]
+
+// P: Give a positive num (also 0 prolly). It wants us to break up that number into an array where each num is an element. Then it wants us to reverse that array and return it. Can only be given nums
+// Return an array of reversed integers (from the og input)
+//Examples
+
+console.log(reversedArray(35231),[1,3,2,5,3])
+console.log(reversedArray(0),[0])
+console.log(reversedArray(888),[8,8,8])
+
+//P: 
+// Make num a string
+//Split string into array
+// Reverse array
+// Return reversed array
+
+function reversedArray(num){
+    let arr = (String(num).split('').reverse(''))
+    return arr.map(n=>Number(n))
+}
