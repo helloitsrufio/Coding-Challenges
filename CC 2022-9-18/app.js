3590eb2bf90068d59f85436e013a4e4ea27599ce
Ruth Reed <ruthreed.dev@gmail.com>
2022-09-19 10:02:51 -0600

    Add url for coding challenge

--- CC 2022-9-18/app.js
+++ CC 2022-9-18/app.js
@@ -1,3 +1,6 @@
+//https://www.codewars.com/kata/5583090cbe83f4fd8c000051/
+
+
 // Convert number to reversed array of digits
 
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
